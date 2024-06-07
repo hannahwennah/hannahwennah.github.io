5d7e253421df74ff692cc5d71c74f60cf5a06fe6
hannahwennah <hannah.wen@jpschools.org>
2024-06-07 14:26:45 +0000

    add platformer.js file

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -1,9 +1,8 @@
 $(function () {
-  // initialize canvas and context when able to
+  // initializes the canvas and context when able to
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
-
   function setup() {
     if (firstTimeSetup) {
       halleImage = document.getElementById("player");
@@ -12,21 +11,15 @@ $(function () {
       $(document).on("keydown", handleKeyDown);
       $(document).on("keyup", handleKeyUp);
       firstTimeSetup = false;
-      //start game
+      // starts the game
       setInterval(main, 1000 / frameRate);
     }
-    //create walls
-    createPlatform(-50, -50, canvas.width + 100, 50); //top
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); //right
-    createPlatform(-50, -50, 50, canvas.height + 500); //bottom
-    createPlatform(canvas.width, -50, 50, canvas.height + 100);
-
-    /*
-     * Uncomment the loops below to add a "grid" to your platformer game's screen
-     * The grid will place both horizontal and vertical platforms incremented 100 pixels apart
-     * This can give you a better idea of where to create new platforms
-     * Comment the lines out to remove the grid
-     */
+    // creates walls
+    createPlatform(-50, -50, canvas.width + 100, 50); // top
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); // right
+    createPlatform(-50, -50, 50, canvas.height + 500); // bottom
+    createPlatform(canvas.width, -50, 50, canvas.height + 100); // left
+    // creates a grid
     /*
     for (let i = 100; i < canvas.width; i += 100) {
       createPlatform(i, canvas.height, -1, -canvas.height);
@@ -35,25 +28,16 @@ $(function () {
       createPlatform(canvas.width, i, -canvas.width, -1);
     }
     */
-    /////////////////////////////////////////////////
-    //////////ONLY CHANGE BELOW THIS POINT///////////
-    /////////////////////////////////////////////////
-
-    // TODO 1
-    // Create platforms
-    // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height); change height later
+    // todo #1: create platforms
+    // example usage: createPlatform(x, y, width, height);
     createPlatform(1150, 150, 25, 10);
     createPlatform(475, 250, 425, 10);
     createPlatform(125, 375, 175, 10);
     createPlatform(500, 500, 200, 10);
     createPlatform(1050, 575, 350, 10);
     createPlatform(0, 625, 400, 10);
-    // TODO 2
-    // Create collectables, at least 3
-    // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
-    // Your collectable choices are 'database' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce) - gravity how fast collectable accelerates downward, bounce strength (how hard object bounces, set to 1 to bounce back to same height)
+    // todo #2: create collectables
+    // example usage: createCollectable(type, x, y, gravity, bounce);
     createCollectable("star", 1141, 75, 0);
     createCollectable("star", 550, 175, 0);
     createCollectable("star", 625, 175, 0);
@@ -79,20 +63,12 @@ $(function () {
     createCollectable("star", 1175, 675, 0);
     createCollectable("star", 1250, 675, 0);
     createCollectable("star", 1325, 675, 0);
-    // TODO 3
-    // Create cannons, at least 3
-    // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
-    // Your wall choices are: 'top' 'left' 'right' and 'bottom'
-    // example usage: createCannon(side, position, delay, width, height)
-    // position is how far along the side
+    // todo #3: create cannons
+    // example usage: createCannon(side, position, delay, width, height);
     createCannon("right", 225, 1500);
     createCannon("right", 400, 1750);
     createCannon("right", 575, 1500);
     createCannon("right", 750, 2250);
-    /////////////////////////////////////////////////
-    //////////ONLY CHANGE ABOVE THIS POINT///////////
-    /////////////////////////////////////////////////
   }
-
   registerSetup(setup);
 });
