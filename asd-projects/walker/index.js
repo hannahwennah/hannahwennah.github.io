8510015badc49d51bbea039f41b0054d3cd0b4ec
hannahwennah <hannah.wen@jpschools.org>
2025-06-20 16:20:18 +0000

    turned walkers into an array of obj

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -7,6 +7,22 @@ function runProgram() {
   const MILLISECONDS_PER_FRAME = 1000 / FRAMES_PER_SECOND;
 
   // variables
+  var walkers = [{
+    height: $("#walker1").height(),
+    speedX: 0,
+    speedY: 0,
+    width: $("#walker1").width(),
+    x: 0,
+    y: 0
+  }, {
+    height: $("#walker2").height(),
+    speedX: 0,
+    speedY: 0,
+    width: $("#walker2").width(),
+    x: $("#board").width() - $("#walker2").width(),
+    y: $("#board").height() - $("#walker2").height()
+  }];
+  /*
   var walker1 = {
     height: $("#walker1").height(),
     speedX: 0,
@@ -24,6 +40,7 @@ function runProgram() {
     y: $("#board").height() - $("#walker2").height()
   };
 
+  */
   // setup
   $(document).on("keydown", handleKeyDown);
   $(document).on("keyup", handleKeyUp);
@@ -41,33 +58,33 @@ function runProgram() {
 
   function handleKeyDown(event) {
     if (event.key === "a") {
-      walker1.speedX -= 5;
+      walkers[0].speedX -= 5;
     } else if (event.key === "w") {
-      walker1.speedY -= 5;
+      walkers[0].speedY -= 5;
     } else if (event.key === "d") {
-      walker1.speedX += 5;
+      walkers[0].speedX += 5;
     } else if (event.key === "s") {
-      walker1.speedY += 5;
+      walkers[0].speedY += 5;
     } else if (event.key === "ArrowLeft") {
-      walker2.speedX -= 5;
+      walkers[1].speedX -= 5;
     } else if (event.key === "ArrowUp") {
-      walker2.speedY -= 5;
+      walkers[1].speedY -= 5;
     } else if (event.key === "ArrowRight") {
-      walker2.speedX += 5;
+      walkers[1].speedX += 5;
     } else if (event.key === "ArrowDown") {
-      walker2.speedY += 5;
+      walkers[1].speedY += 5;
     }
   }
 
   function handleKeyUp(event) {
     if (event.key === "a" || event.key === "d") {
-      walker1.speedX = 0;
+      walkers[0].speedX = 0;
     } else if (event.key === "w" || event.key === "s") {
-      walker1.speedY = 0;
+      walkers[0].speedY = 0;
     } else if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-      walker2.speedX = 0;
+      walkers[1].speedX = 0;
     } else if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-      walker2.speedY = 0;
+      walkers[1].speedY = 0;
     }
   }
 
@@ -94,42 +111,42 @@ function runProgram() {
   }
 
   function redrawGameItem() {
-    $("#walker1").css("left", walker1.x).css("top", walker1.y);
-    $("#walker2").css("left", walker2.x).css("top", walker2.y);
+    $("#walker1").css("left", walkers[0].x).css("top", walkers[0].y);
+    $("#walker2").css("left", walkers[1].x).css("top", walkers[1].y);
   }
 
   function repositionGameItem() {
-    walker1.x += walker1.speedX;
-    walker1.y += walker1.speedY;
-    walker2.x += walker2.speedX;
-    walker2.y += walker2.speedY;
+    walkers[0].x += walkers[0].speedX;
+    walkers[0].y += walkers[0].speedY;
+    walkers[1].x += walkers[1].speedX;
+    walkers[1].y += walkers[1].speedY;
     wallCollision();
   }
   // review rubric
   function wallCollision() {
-    if (walker1.x < 0) {
-      walker1.x = 0;
+    if (walkers[0].x < 0) {
+      walkers[0].x = 0;
     }
-    if (walker1.x > $("#board").width() - walker1.width) {
-      walker1.x = $("#board").width() - walker1.width;
+    if (walkers[0].x > $("#board").width() - walkers[0].width) {
+      walkers[0].x = $("#board").width() - walkers[0].width;
     }
-    if (walker1.y < 0) {
-      walker1.y = 0;
+    if (walkers[0].y < 0) {
+      walkers[0].y = 0;
     }
-    if (walker1.y > $("#board").height() - walker1.height) {
-      walker1.y = $("#board").height() - walker1.height;
+    if (walkers[0].y > $("#board").height() - walkers[0].height) {
+      walkers[0].y = $("#board").height() - walkers[0].height;
     }
-    if (walker2.x < 0) {
-      walker2.x = 0;
+    if (walkers[1].x < 0) {
+      walkers[1].x = 0;
     }
-    if (walker2.x > $("#board").width() - walker2.width) {
-      walker2.x = $("#board").width() - walker2.width;
+    if (walkers[1].x > $("#board").width() - walkers[1].width) {
+      walkers[1].x = $("#board").width() - walkers[1].width;
     }
-    if (walker2.y < 0) {
-      walker2.y = 0;
+    if (walkers[1].y < 0) {
+      walkers[1].y = 0;
     }
-    if (walker2.y > $("#board").height() - walker2.height) {
-      walker2.y = $("#board").height() - walker2.height;
+    if (walkers[1].y > $("#board").height() - walkers[1].height) {
+      walkers[1].y = $("#board").height() - walkers[1].height;
     }
   }
 }
